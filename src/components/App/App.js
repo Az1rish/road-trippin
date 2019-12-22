128b0191b30768d5c7cf21bfc6d10ac36235fc07
Shane <shanemcneil19@gmail.com>
2019-12-22 07:44:42 -0700

    create Nav and Homepage components with test files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Nav from '../Nav/Nav'
+import HomePage from '../../routes/Homepage'
 
-function App() {
-  return (
-    <main className="App">
-      {/* content goes here */}
-    </main>
-  );
+export default class App extends Component {
+  state = { hasError: false }
+
+  static getDerivedStateFromError(error) {
+    console.error(error)
+    return { hasError: true }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <nav className='App__navBar'>
+          <Nav />
+        </nav>
+        <main className='App__main'>
+          {this.state.hasError && <p className='red'>I'm sorry, it appears there is an error.</p>}
+          <Switch>
+            <Route
+              exact
+              path={'/'}
+              component={HomePage}
+            />
+          </Switch>
+        </main>
+      </div>
+    )
+  }
 }
-
-export default App;
