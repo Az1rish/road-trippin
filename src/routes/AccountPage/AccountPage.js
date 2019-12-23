2c820b446e798ff9879b48cb4ac97706072476dc
Shane <shanemcneil19@gmail.com>
2019-12-23 07:01:45 -0700

    create files for AccountPage

---
+++ src/routes/AccountPage/AccountPage.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import PhotoListContext from '../../contexts/PhotoListContext'
+import PhotoApiService from '../../services/photo-api-service'
+import { Section } from '../../components/Utils/Utils'
+import Photo from '../../components/Photo/Photo'
+import './AccountPage.css'
+
