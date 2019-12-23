e8cffc33e441add8d0b502d731bb4f60e5d849e1
Shane <shanemcneil19@gmail.com>
2019-12-23 06:41:53 -0700

    complete PrivateRoute

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Nav from '../Nav/Nav'
 import PublicOnlyRoute from '../Utils/PublicOnlyRoute'
+import PrivateRoute from '../Utils/PrivateRoute'
 import HomePage from '../../routes/Homepage/HomePage'
 import LoginPage from '../../routes/LoginPage/LoginPage'
+import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage'
 
 export default class App extends Component {
   state = { hasError: false }
@@ -34,6 +36,25 @@ export default class App extends Component {
             <PublicOnlyRoute
               path={'/register'}
               component={RegistrationPage}
+            />
+            <PrivateRoute
+              path={'/account/:accountId'}
+              component={AccountPage}
+            />
+            <PrivateRoute
+              path={'/account/:accountId/myPhotos'}
+              component={MyPhotosPage}
+            />
+            <PrivateRoute
+              path={'/account/:accountId/photo/:photoId'}
+              component={PhotoPage}
+            />
+            <PrivateRoute
+              path={'/account/:accountId/upload'}
+              component={UploadPage}
+            />
+            <Route
+              component={NotFoundPage}
             />
           </Switch>
         </main>
