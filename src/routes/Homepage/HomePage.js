59422c52e3893301e9333022867509822fc64662
Shane <shanemcneil19@gmail.com>
2019-12-24 08:21:03 -0700

    make progress on HomePage, add styling

--- src/routes/Homepage/HomePage.js
+++ src/routes/Homepage/HomePage.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
+import { Section } from '../../components/Utils/Utils'
+import LoginForm from '../../components/LoginForm/LoginForm'
+import './HomePage.css'
 
 export default class HomePage extends Component {
+    renderCards() {
+        const cards = [
+            "View photos and descriptions from locations around the world",
+            "Upload your own photos for others to view",
+            "Join the discussion with the Road Trippin' community",
+            "Decide which wonderful place you want to visit!"
+        ]
+        return cards.map(card =>
+            <Section className="card">
+                <p>{card}</p>
+            </Section> 
+        )
+    }
+
     render() {
         return (
             <div>
-
+                <h2>Helping everyone see the world</h2>
+                <div className="cards">
+                    {this.renderCards()}
+                </div>
+                <LoginForm
+                    className='homePageLogin'
+                />
             </div>
         ) 
     }
