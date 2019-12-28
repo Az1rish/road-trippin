ac31e05ab0f916bc66296921b87468bef7edf0fd
Shane <shanemcneil19@gmail.com>
2019-12-28 05:40:12 -0700

    update wireframe Account page, move src2 file

--- src/routes/AccountPage/AccountPage.js
+++ src/routes/AccountPage/AccountPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import PhotoListContext from '../../contexts/PhotoListContext'
-import PhotoApiService from '../../services/photo-api-service'
+// import PhotoListContext from '../../contexts/PhotoListContext'
+// import PhotoApiService from '../../services/photo-api-service'
 import { Section } from '../../components/Utils/Utils'
 import Photo from '../../components/Photo/Photo'
 import './AccountPage.css'
