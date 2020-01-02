1f88231e3f882032b057caf2d1eb6a05114751ab
Shane <shanemcneil19@gmail.com>
2020-01-02 06:51:50 -0700

    add context to Account page

--- src/routes/AccountPage/AccountPage.js
+++ src/routes/AccountPage/AccountPage.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
-// import PhotoListContext from '../../contexts/PhotoListContext'
-// import PhotoApiService from '../../services/photo-api-service'
+import PhotoListContext from '../../contexts/PhotoListContext'
+import PhotoApiService from '../../services/photo-api-service'
 import { Section } from '../../components/Utils/Utils'
 import Photo from '../../components/Photo/Photo'
 import './AccountPage.css'
-import photoList from '../../store'
+// import photoList from '../../store'
 import AccountButtons from '../../components/AccountButtons/AccountButtons'
 
-export default class AccountPage extends Component {
+export default class AccountPage extends Component {    
+    static contextType = PhotoListContext
+
+    componentDidMount() {
+        this.context.clearError()
+        PhotoApiService.getPhotos()
+            .then(this.context.setPhotoList)
+            .catch(this.context.setError)
+    }
+
     renderPhotos() {
+        const { photoList = [] } = this.context
         return photoList.map(photo =>
                 <Photo
                     key={photo.id}
