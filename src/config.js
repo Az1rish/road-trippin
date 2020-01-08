7536446ab7903be4311e43e920707b294f75d89b
Shane <shanemcneil19@gmail.com>
2020-01-07 19:14:44 -0700

    add /api to API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://road-trippin-api.herokuapp.com',
+    API_ENDPOINT: 'https://road-trippin-api.herokuapp.com/api',
     TOKEN_KEY: 'road-trippin-client-auth-token',
 }
