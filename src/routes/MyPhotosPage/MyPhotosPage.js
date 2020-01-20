50584cbf8a5090459ec867b93fdbcc5b8dbac656
Shane <shanemcneil19@gmail.com>
2020-01-19 19:46:44 -0700

    try to have two context consumers in one component

--- src/routes/MyPhotosPage/MyPhotosPage.js
+++ src/routes/MyPhotosPage/MyPhotosPage.js
@@ -3,44 +3,52 @@ import PhotoListContext from '../../contexts/PhotoListContext'
 import PhotoApiService from '../../services/photo-api-service'
 import { Section } from '../../components/Utils/Utils'
 import Photo from '../../components/Photo/Photo'
-// import AuthContext from '../../contexts/AuthContext'
+import AuthContext from '../../contexts/AuthContext'
 // import TokenService from '../../services/token-service'
 import './MyPhotosPage.css'
 // import photoList from '../../store'
 // import AccountButtons from '../../components/AccountButtons/AccountButtons'
 
 export default class MyPhotosPage extends Component {
-    static contextType = PhotoListContext
-
-    componentDidMount() {
-        console.log(this.context)
-        this.context.clearError()
-        // PhotoApiService.getPhotosByUser(this.context.user)
-        PhotoApiService.getPhotos()
-            .then(this.context.setPhotoList)
-            .catch(this.context.setError)
-    }
-
-    renderPhotos() {
-        const { photoList = [] } = this.context
-        return photoList.map(photo =>
-                <Photo
-                    key={photo.id}
-                    photo={photo}/>
-                )
-    }
+    // static contextType = PhotoListContext
 
     render() {
-        const { error } = this.context
+        // 
         return (
-            <>
-                {/* <AccountButtons /> */}
-                <Section list className='MyPhotosPage'>
-                    {error
-                        ? <p className='red'>There was an error, try again</p>
-                        : this.renderPhotos()}
-                </Section>
-            </>
+            <AuthContext.Consumer>{(authContext) => {
+                <PhotoListContext.Consumer>{(photoListContext) => {
+                    componentDidMount() {
+                        // console.log(this.context)
+                        this.photoListContext.clearError()
+                        PhotoApiService.getPhotosByUser(this.authContext.user)
+                        // PhotoApiService.getPhotos()
+                            .then(this.photoListContext.setPhotoList)
+                            .catch(this.photoListContext.setError)
+                    }
+                
+                    renderPhotos() {
+                        const { photoList = [] } = this.photoListContext
+                        return photoList.map(photo =>
+                                <Photo
+                                    key={photo.id}
+                                    photo={photo}/>
+                                )
+                    }
+
+                    return(
+                        const { error } = this.context
+                        <>
+                            {/* <AccountButtons /> */}
+                            <Section list className='MyPhotosPage'>
+                                {error
+                                    ? <p className='red'>There was an error, try again</p>
+                                    : this.renderPhotos()}
+                            </Section>
+                        </> 
+                    )
+                }}</PhotoListContext.Consumer>
+            }}</AuthContext.Consumer>
+            
         )
     }
 }
