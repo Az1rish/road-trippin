1eec89317dc365af3364c5bf8ba98a98abe73a08
Shane <shanemcneil19@gmail.com>
2020-02-06 21:07:59 -0700

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 export default function Footer() {
     return (
         <footer className='Footer'>
-            &copy; Shane McNeil 2019
+            &copy; Shane McNeil 2020
         </footer>
     )
 }
