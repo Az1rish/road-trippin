66f8d219708a581c3ec45d88222abc085b7a415e
Shane <shanemcneil19@gmail.com>
2020-02-06 21:06:26 -0700

    add 'sign in as guest' button to homepage

--- src/routes/Homepage/HomePage.js
+++ src/routes/Homepage/HomePage.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
-import { Section } from '../../components/Utils/Utils'
+import { Section, Button } from '../../components/Utils/Utils'
+import AuthApiService from '../../services/auth-api-service'
+import TokenService from '../../services/token-service'
+import AuthContext from '../../contexts/AuthContext'
 import './HomePage.css'
 import { Link } from 'react-router-dom'
 
 export default class HomePage extends Component {
+    static defaultProps = {
+        onLoginSuccess: () => {}
+    }
+
+    static contextType = AuthContext
+
+    state = {
+        error: null,
+    }
+
+    handleGuestSignIn = ev => {
+        ev.preventDefault()
+        const credentials = {
+            user_name: 'DemoUser',
+            password: 'A1$password'
+        }
+
+        AuthApiService.postLogin(credentials)
+            .then((res) => {
+                this.context.setUser(credentials.user_name)
+                TokenService.saveAuthToken(res.authToken)
+                this.props.history.push('/home')
+            })
+            .catch(res => {
+                this.setState({ error: res.error })
+            })
+        
+    }
+
     renderCards() {
         const cards = [
             "View photos and descriptions from locations around the world",
@@ -38,6 +70,12 @@ export default class HomePage extends Component {
                     to='/register'>
                         Create an account
                 </Link>
+                <form 
+                    className='guestSignIn'
+                    onSubmit={this.handleGuestSignIn}>
+                    <p className='question'>No time to register an account?</p>
+                    <Button type='submit'>Sign in as a guest</Button>
+                </form>
             </div>
         ) 
     }
